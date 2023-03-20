22c66b310643a80cc69b5136d707816aed141309
Aryan Indarapu <aryan1pro@gmail.com>
2023-03-20 13:28:11 -0500

    added raspi commands

--- lab2_sp23/raspi_commands.py
+++ lab2_sp23/raspi_commands.py
@@ -1,4 +1,15 @@
 import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_val_images, train_val_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# data preprocessing to normalize data
+test_images = test_images / 255.0
+
+# reshape data
+test_images = test_images[..., np.newaxis]
 
 # Load the TFLite model and allocate tensors.
 interpreter = tf.lite.Interpreter(model_path="my_model.tflite")
@@ -10,7 +21,7 @@ output_details = interpreter.get_output_details()
 
 # Test the model on random input data.
 input_shape = input_details[0]['shape']
-interpreter.set_tensor(input_details[0]['index'], input_data)
+interpreter.set_tensor(input_details[0]['index'], test_images)
 
 # Run the model
 interpreter.invoke()
